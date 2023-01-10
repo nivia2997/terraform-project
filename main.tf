f41e38cd5ebb9b7178ea3abcacfaeb127d5eb927
EC2 Default User <ec2-user@ip-172-31-39-43.us-east-2.compute.internal>
2023-01-10 03:55:15 +0000

    actualizado

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "test_vpc" {
+  cidr_block = var.cidr_block
+
+ 
+tags = {
+    Name = "vpc_proyecto"
+  }
+}
+
+resource "aws_subnet" "subred_publica" {
+  vpc_id                  = aws_vpc.test_vpc.id
+  cidr_block              = var.cidr_block_subnet
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+
+  tags = {
+    Name = "subred_publica"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "asignar una region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cidr_block" {
+  description = "asignar un cidr block al vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "cidr_block_subnet" {
+  description = "asignar cidr block para el subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "eligiendo una zona de disponibilidad"
+  type        = string
+  default     = "us-east-1a"
+}
