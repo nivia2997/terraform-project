de09e1bffe39074c7f5a479635fb74ccafe1820f
EC2 Default User <ec2-user@ip-172-31-39-43.us-east-2.compute.internal>
2023-01-11 02:13:48 +0000

    actualización 3

--- main.tf
+++ main.tf
@@ -28,3 +28,31 @@ resource "aws_subnet" "subred_terraform" {
     Name = "subred_terraform"
   }
 }
+
+resource "aws_internet_gateway" "igw_test_vpc" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = var.Name_internet_gateway
+  }
+}
+
+resource "aws_route_table" "rt_nginx" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = var.Name_internet_gateway
+  }
+}
+
+resource "aws_route" "route_nginx" {
+  route_table_id         = aws_route_table.rt_nginx.id
+  destination_cidr_block = var.destination_cidr_block
+  gateway_id             = aws_internet_gateway.igw_test_vpc.id
+}
+
+resource "aws_route_table_association" "rt_ass_nginx" {
+  subnet_id      = aws_subnet.subred_publica.id
+  route_table_id = aws_route_table.rt_nginx.id
+}
+
