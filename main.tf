82dd1dc4fe3efcbd75b5e6c305d241d068ee0194
EC2 Default User <ec2-user@ip-172-31-39-43.us-east-2.compute.internal>
2023-01-11 03:17:33 +0000

    actualización 6

--- main.tf
+++ main.tf
@@ -18,17 +18,6 @@ resource "aws_subnet" "subred_publica" {
   }
 }
 
-resource "aws_subnet" "subred_terraform" {
-  vpc_id                  = aws_vpc.test_vpc.id
-  cidr_block              = var.cidr_block_subnet
-  map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
-
-  tags = {
-    Name = "subred_terraform"
-  }
-}
-
 resource "aws_internet_gateway" "igw_test_vpc" {
   vpc_id = aws_vpc.test_vpc.id
 
