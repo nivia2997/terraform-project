bf012c10b5769356266903069ecc51f27451f5de
EC2 Default User <ec2-user@ip-172-31-39-43.us-east-2.compute.internal>
2023-01-17 01:10:39 +0000

    actualización 14

--- datasources.tf
+++ datasources.tf
@@ -7,3 +7,8 @@
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform_key"
+  public_key = file("/home/ec2-user/.ssh/terraform_key.pub")
+}
